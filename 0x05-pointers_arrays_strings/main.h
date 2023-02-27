c57eab02ec011d88c4d55199bd3fcfeb02585744
ufarukamin <ufarukamin@gmail.com>
2023-02-27 15:40:51 -0800

    a function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,3 +9,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _strlength(char *s);
+int char_val(char c);
+int _atoi_helper(char c, int places);
+int _atoi(char *s);
