7091e9803fc8b771016cddbf3e41a9125460435f
ufarukamin <ufarukamin@gmail.com>
2023-03-20 17:50:15 -0700

    struct dog update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,15 @@
 #include "dog.h"
+
+/**
+ * init_dog - a function that initialize a variable of type struct dog
+ * @d: struct dog
+ * @name: string
+ * @age: number age
+ * @owner: string
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,12 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -  a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct dog
 {
 	char *name;
