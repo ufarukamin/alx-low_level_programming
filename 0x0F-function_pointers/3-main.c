8a8e9d4dccaab6b2ef64a33df5a8bd863be6aff6
ufarukamin <ufarukamin@gmail.com>
2023-03-23 06:52:36 -0700

    a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0, 98, 99 0r 100
+ */
+int main(int argc, char *argv[])
+{
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && (*argv[3] == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds a and b
+ * @a: an integer
+ * @b: an integer
+ * Return: sum of a and b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract b from a
+ * @a: an integer
+ * @b: an integer
+ * Return: a - b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply a by b
+ * @a: an integer
+ * @b: an integer
+ * Return: a * b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide a by b
+ * @a: an integer
+ * @b: an integer
+ * Return: a / b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - finds the reminder of division of a by b
+ * @a: an integer
+ * @b: an integer
+ * Return: a mod b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
