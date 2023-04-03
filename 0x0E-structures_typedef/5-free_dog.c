1b69da678b537e9587f37de490e9b9cab0f3373f
ufarukamin <ufarukamin@gmail.com>
2023-04-03 07:18:09 -0700

    a function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog - Frees a dog struct
+* @d: dog to free
+*/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
