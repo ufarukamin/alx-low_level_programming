4f10af22f9bcfdabe6b6d172d530319ad0649bc0
ufarukamin <ufarukamin@gmail.com>
2023-04-04 10:06:32 -0700

    a function that inserts a new node at a given position.

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* sum_listint - returns the sum of all the
+* data (n) of a listint_t linked list.
+* @head: the head of the listint_t list.
+* Return: If the list is empty - 0,
+* otherwise - the sum of all the data.
+*/
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: pointer to the start of the list
+ * @index: index to add the node at
+ * @n: value to assign to the data of the node
+ * Return: address of the new node, or NULL if it fails or a node cannot be
+ * added at the given index
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 1;
+	listint_t *new_node, *temp;
+
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	temp = *head;
+	while (i != idx)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		i++;
+	}
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
+}
